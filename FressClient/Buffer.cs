96bef4ef0e2c80fe573d44513838a31e2478f7b4
Tyler Schicke <tyler_schicke@brown.edu>
2018-12-03 23:44:16 -0500

    initial commit

---
+++ FressClient/Buffer.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace FressClient
+{
+    public class Buffer : Drawable
+    {
+        private readonly Text _drawableText;
+        private RectangleShape _cursor;
+        private int _cursorIndex;
+
+        public int CharacterWidth;
+
+        public uint CurrentChar
+        {
+            get
+            {
+                if (_cursorIndex < BufferText.Length)
+                {
+                    var curChar = BufferText[_cursorIndex];
+                    if (curChar == '\n' || curChar == '\t') return ' ';
+                    return curChar;
+                }
+
+                return ' ';
+            }
+        }
+
+        public Buffer(int characterWidth)
+        {
+            _drawableText = new Text(BufferText, Program.Font, Program.FontSize);
+            _cursor = new RectangleShape() { FillColor = new Color(Color.White) };
+            CharacterWidth = characterWidth;
+        }
+
+        public string BufferText { get; set; } = "";
+
+        public void Draw(RenderTarget target, RenderStates states)
+        {
+            var size = _drawableText.Font.GetGlyph('a', _drawableText.CharacterSize, false, 0).Bounds;
+            float height = _drawableText.Font.GetLineSpacing(_drawableText.CharacterSize);
+            _cursor.Size = new Vector2f(size.Width, height - 1);
+
+
+            int cursorIndex = _cursorIndex;
+            var lines = BufferText.Split("\n");
+            Vector2f? cursorPos = null;
+            Vector2f currentPosition = new Vector2f(0, 0);
+            foreach (var line in lines)
+            {
+                var drawable = line;
+                Vector2f? pos;
+                while (drawable.Length > CharacterWidth)
+                {
+                    var l = drawable.Substring(0, CharacterWidth);
+                    drawable = drawable.Substring(CharacterWidth);
+                    pos = DrawString(l, target, currentPosition, cursorIndex);
+                    cursorPos = cursorPos ?? pos;
+                    cursorIndex -= l.Length;
+                    currentPosition.Y += height;
+                }
+                pos = DrawString(drawable, target, currentPosition, cursorIndex);
+                cursorPos = cursorPos ?? pos;
+                cursorIndex -= drawable.Length + 1;
+                currentPosition.Y += height;
+            }
+
+            if (cursorPos == null)
+            {
+                Vector2f p = _drawableText.FindCharacterPos((uint)_drawableText.DisplayedString.Length);
+                cursorPos = new Vector2f(p.X, currentPosition.Y - height);
+            }
+            _cursor.Position = new Vector2f(cursorPos.Value.X, cursorPos.Value.Y + 1);
+            target.Draw(_cursor);
+        }
+
+        private Vector2f? DrawString(string s, RenderTarget target, Vector2f position, int cursorPos)
+        {
+            _drawableText.DisplayedString = s;
+            _drawableText.Position = position;
+            target.Draw(_drawableText);
+            if (cursorPos <= s.Length && cursorPos >= 0)
+            {
+                var characterPos = _drawableText.FindCharacterPos((uint) cursorPos);
+                return new Vector2f(characterPos.X, position.Y);
+            }
+
+            return null;
+        }
+
+        public void HandleText(TextEventArgs args)
+        {
+            var newChar = args.Unicode;
+            if (newChar == "\b")
+            {
+                Backspace();
+                return;
+            }
+            if (newChar == "\r") newChar = "\n";
+            BufferText = BufferText.Insert(_cursorIndex, newChar);
+            CursorRight();
+        }
+
+        public void Backspace()
+        {
+            var str = BufferText;
+            if (str.Length > 0 && _cursorIndex > 0)
+            {
+                BufferText = str.Remove(_cursorIndex - 1, 1);
+                CursorLeft();
+            }
+        }
+
+        public void CursorLeft()
+        {
+            _cursorIndex = Math.Max(0, _cursorIndex - 1);
+        }
+
+        public void CursorRight()
+        {
+            _cursorIndex = Math.Min(BufferText.Length, _cursorIndex + 1);
+        }
+    }
+}
---
+++ FressClient/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Specialized;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using Rebex.Net;
+using Rebex.TerminalEmulation;
+using SFML.Graphics;
+using SFML.System;
+using SFML.Window;
+
+namespace FressClient
+{
+    class Program
+    {
+
+        static async Task Main(string[] args)
+        {
+            Rebex.Licensing.Key = "==AKOz7Fgv0W1Kau2iJwlo61vuaQ1v05EfMcFXUgg6T5rQ==";
+            await Run(args);
+        }
+
+        public static Font Font;
+        public static readonly uint FontSize = 14;
+
+        public static Buffer MainBuffer;
+
+        static async Task Run(string[] args)
+        {
+            var window = new RenderWindow(new VideoMode(640, 480), "FRESS");
+            window.KeyPressed += WindowOnKeyPressed;
+            window.TextEntered += Window_TextEntered;
+            window.Closed += WindowOnClosed;
+            Font = new Font("resources/courier.ttf");
+
+            MainBuffer = new Buffer(30);
+
+            while (window.IsOpen)
+            {
+                window.Clear(new SFML.Graphics.Color(0, 0, 30));
+                window.DispatchEvents();
+
+                window.Draw(MainBuffer);
+
+                window.Display();
+            }
+
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Not enough arguments");
+                Console.Read();
+                return;
+            }
+
+            var ip = args[0];
+            var port = int.Parse(args[1]);
+            var server = new Telnet(ip, port);
+            var scripting = server.StartScripting();
+            scripting.Timeout = 300;
+
+            var buffer = new byte[1024];
+            Console.WriteLine("Hello World!");
+            string line;
+            while ((line = Console.ReadLine()) != null)
+            {
+                if(!string.IsNullOrWhiteSpace(line))
+                    scripting.Send(line);
+                var res = scripting.ReadUntil(ScriptEvent.Timeout);
+                if (!string.IsNullOrEmpty(res))
+                {
+                    Console.WriteLine(res);
+                }
+            }
+        }
+
+        private static void WindowOnKeyPressed(object sender, KeyEventArgs keyEventArgs)
+        {
+            switch (keyEventArgs.Code)
+            {
+                case Keyboard.Key.Left:
+                    MainBuffer.CursorLeft();
+                    break;
+                case Keyboard.Key.Right:
+                    MainBuffer.CursorRight();
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private static void Window_TextEntered(object sender, TextEventArgs e)
+        {
+            MainBuffer.HandleText(e);
+        }
+
+        private static void WindowOnClosed(object sender, EventArgs eventArgs)
+        {
+            (sender as Window)?.Close();
+        }
+    }
+}
